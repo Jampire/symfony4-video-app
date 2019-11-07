e74bf7e7712362536f189db609d96411f7322c71
Dzianis Kotau <jampire.blr@gmail.com>
2019-11-08 02:00:34 +0300

    Step 122

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -21,4 +21,23 @@ class FrontController extends AbstractController
     {
         return $this->render('front/index.html.twig');
     }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList(): Response
+    {
+        $sampleVideos = [289729765,238902809,150870038,219727723,289879647,261379936,289029793,60594348,290253648];
+        return $this->render('front/video_list.html.twig', [
+            'sample_videos' => $sampleVideos,
+        ]);
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails(): Response
+    {
+        return $this->render('front/video_details.html.twig');
+    }
 }
